bf9a7355f229a34a0b6e332fd7cf90bddfa5325c
Lucas William <lucaswilliameufrasio@gmail.com>
2020-01-16 05:42:13 -0300

    Initial Commit of Web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -12,6 +13,8 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-pfahd.mongodb.net/
     useCreateIndex: true
 });
 
+// app.use(cors({ origin: 'http://localhost:7777' }));
+app.use(cors());
 app.use(express.json());
 
 // Métodos HTTP: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS
